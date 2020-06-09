573afa342bcfa9c5182b20c914ea3d002cbcbee4
D <dyeshiamonet@gmail.com>
2020-06-09 18:07:25 -0500

    import pic and wrap in a figure tag to display

---
+++ src/components/ContactCard/Pic.js
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+export const Pic = ({ name, pic }) => (
+  <figure>
+    <img src={pic.large} alt="" />
+    <figcaption>{name.first}</figcaption>
+  </figure>
+);
+Pic.propTypes = {
+  pic: PropTypes.object.isRequired,
+  name: PropTypes.object.isRequired,
+};
--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -1,1 +1,16 @@
-export { ContactCard } from "./Contact/Card";
+import PropTypes from "prop-types";
+import React from "react";
+
+import { Pic } from "./Pic";
+
+export class ContactCard extends React.Component {
+  static propTypes = {
+    cell: PropTypes.string,
+    email: PropTypes.string,
+    name: PropTypes.object,
+    picture: PropTypes.object,
+  };
+  render() {
+    return <Pic name={this.props.name} pic={this.props.picture} />;
+  }
+}
